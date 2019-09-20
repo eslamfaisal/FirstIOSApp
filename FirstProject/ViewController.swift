7ebc8119d8156676c8992f264bcef61685652662
eslamfaisal <eslamfaisal402@yahoo.cm>
2019-09-20 13:03:58 +0200

    dicee app

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var firtBall: UIImageView!
+    @IBOutlet weak var secondBall: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func actinBtn(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(3))
+        if(randomDiceIndex1 == 1){
+            secondBall.image = UIImage(named: "dice1")
+        }else if(randomDiceIndex1 == 2){
+            secondBall.image = UIImage(named: "dice2")
+        }else{
+            secondBall.image = UIImage(named: "dice3")
+        }
+        
+    }
 
 }
 
